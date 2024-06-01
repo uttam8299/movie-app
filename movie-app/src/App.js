cea84019771d787e16f5b7592c4948513cfb9b30
uttam8299 <uttam.yadav8299@gmail.com>
2024-06-01 19:52:20 +0530

    api call & basic search box

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useEffect, useState } from "react";
+import MovieList from "./components/MovieList";
+import { Container, Row } from "react-bootstrap";
+import MovieHeader from "./components/MovieHeader";
+import SearchBox from "./components/SearchBox";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovies = async (searchValue) => {
+    console.log(searchValue);
+    var url = `http://www.omdbapi.com/?s=${searchValue}&apikey=e3ac1d4c`;
+    var response = await fetch(url);
+    var responseJson = await response.json();
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovies(searchValue);
+  }, [searchValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieHeader heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
